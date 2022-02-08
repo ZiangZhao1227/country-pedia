3ecc03c0dbc67df88e2531547f92f13b541ec92b
ziangz <zhaoziang1227@gmail.com>
2022-02-08 12:09:34 +0200

    first custom hook done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import useCountries from './custom-hooks/useCountries';
 
 function App() {
-  return <div className='App'></div>
+  const [data, error] = useCountries();
+  console.log(error)
+  return <div className='App'>
+    {data && <div>{data}</div>}
+  </div>
 }
 
 export default App
--- src/custom-hooks/useCountries.js
+++ src/custom-hooks/useCountries.js
@@ -1,1 +1,38 @@
-// This hook is used to fetch all countries
+import { useEffect, useState } from "react";
+
+const useCountries = () => {
+  const [countries, setCountries] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const fetchData = async (controller) => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all", {
+          method: "get",
+          signal: controller.signal,
+        });
+        if (response.status !== 200) {
+          throw new Error("cannot fetch the data");
+        }
+        const data = await response.json();
+        const countryListName = data.map(
+            (country) => country.name.common
+        );
+        setCountries(countryListName);
+      } catch (error) {
+        setError(error);
+        console.log(error.message);
+      }
+    };
+    fetchData(controller);
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return [countries, error];
+};
+
+export default useCountries;
